3085c3251673b126fb988745432f466c9ed73e29
Arvikdb17 <128912243+Arvikdb17@users.noreply.github.com>
2024-07-24 14:29:58 +0530

    Update MultipleClassSingleFile.java

--- MultipleClassSingleFile.java
+++ MultipleClassSingleFile.java
@@ -1,14 +1,20 @@
 package Basics;
 
-public class MultipleClassSingleFile {
+public class Main {
     //Create multiple classes in single file and compile and explore how many .class files are generated.
     public static void main(String[] args){
-
+     public static void displayMessage() {
+          System.out.println("This is MainClass.");
+       }
     }
 }
 class A{
-
+     public static void displayMessage() {
+          System.out.println("This is MainClass.");
+       }
 }
 class B{
-
+     public static void displayMessage() {
+          System.out.println("This is MainClass.");
+       }
 }
